fbb8058e18d95a5253e37fc0f9090a99b6f1954a
˜Lucas <lucas.limeres@gmail.com>
2019-06-28 19:47:37 -0300

    Sucrase + Nodemon

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-const { Router } = require("express");
+import { Router } from "express";
 
 const routes = new Router();
 
 routes.get("/", (req, res) => {
-  res.json({ message: "Hello World" });
+  res.json({ message: "Hello Lucas" });
 });
 
-module.exports = routes;
+export default routes;
