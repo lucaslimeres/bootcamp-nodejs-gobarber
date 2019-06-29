e1e9a62ffa30e2559661625513e533607c71722e
˜Lucas <lucas.limeres@gmail.com>
2019-06-29 15:20:12 -0300

    Eslint + Prettier + EditorConfig

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  res.json({ message: "Hello Lucas" });
+routes.get('/', (req, res) => {
+  res.json({ message: 'Hello Lucas' });
 });
 
 export default routes;
