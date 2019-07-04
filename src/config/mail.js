2b6b690e3f4d047d28f641e54b94c7c02754c0be
˜Lucas <lucas.limeres@gmail.com>
2019-07-04 19:26:15 -0300

    Envio de E-mail

---
+++ src/config/mail.js
@@ -0,0 +1,20 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'f4e999d8d6fd64',
+    pass: 'dc0ab8012233b1',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
+// Amazom SES
+// Mailgun
+// Sparkpost
+// Mandril (Mailchimp)
+// GMail
+
+// Mailtrap (DEV)
